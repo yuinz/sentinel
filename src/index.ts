75466aa8509f41a13d954d35ab3dcbc50e6f6395
dwight <dwightsyeve49@gmail.com>
2026-01-16 09:07:45 -0800

    secondg

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.use(morgan('combined', {
 // 2. Global Rate Limiting (Enterprise Grade)
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per window
+    max: 600, // Limit each IP to 100 requests per window
     standardHeaders: true,
     legacyHeaders: false,
     message: { error: 'Too many requests from this IP, please try again later.' }
